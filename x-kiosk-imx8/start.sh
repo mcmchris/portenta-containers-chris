5d2861014c016d9210dfaeb08d288d95c561fca0
Christopher Méndez <mcmchris@hotmail.com>
2024-04-14 17:57:06 -0400

    image update

--- x-kiosk-imx8/start.sh
+++ x-kiosk-imx8/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # wait for display
-if [ ! -e /sys/class/graphics/fbcon ]; then
+if [ ! -e /sys/class/graphics/fb0 ]; then
     echo "Display not detected.  Will wait for it to show up ..."
 fi
-while [ ! -e /sys/class/graphics/fbcon ]; do
+while [ ! -e /sys/class/graphics/fb0 ]; do
     sleep 1
 done
 
